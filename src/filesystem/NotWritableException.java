8386a62cbb928151a70793081088ea65a21b5f1f
Vincent Van Schependom <vincent.van.schependom@gmail.com>
2024-03-15 17:31:46 +0100

    Developed Exception for NotWritable case

---
+++ src/filesystem/NotWritableException.java
@@ -0,0 +1,65 @@
+package filesystem;
+
+import be.kuleuven.cs.som.annotate.*;
+
+/**
+ * A class for signaling illegal attempts to change a file or folder.
+ *
+ * @invar	The referenced file or folder must be effective
+ * 			| isValidItem(getItem())
+ * @author  Vincent Van Schependom
+ * @author  Flor De Meulemeester
+ * @author  Arne Claerhout
+ * @version 2.0
+ */
+public class NotWritableException extends RuntimeException {
+
+    /**
+     * Required because this class inherits from Exception
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Variable referencing the file to which change was denied.
+     */
+    private final Item item;
+
+    /**
+     * Check whether the fiven item is a valid item for this Exception.
+     * @param 	item
+     * 			The item to check
+     * @return	| result == (item != null)
+     *          |   && ( (item instanceof File)
+     *          |       || (item instanceof Folder) )
+     */
+    public static boolean isValidItem(Item item) {
+        return (item != null) &&
+                ((item instanceof File) ||
+                        (item instanceof Folder));
+    }
+
+    /**
+     * Initialize this new item not writable exception involving the
+     * given folder or file.
+     *
+     * @param	item
+     * 			The folder or file for the new not writable exception.
+     * @pre		The given item must be a valid item
+     * 			| isValidItem(item)
+     * @post	The item involved in the new not writable exception
+     * 			is set to the given item.
+     * 			| new.getItem() == item
+     */
+    public NotWritableException(Item item) {
+        this.item = item;
+    }
+
+    /**
+     * Return the item involved in this file not writable exception.
+     */
+    @Basic @Immutable
+    public File getItem() {
+        return item;
+    }
+
+}
